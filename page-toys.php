75d203ffadc153c2a94354c590fc28303bc38847
Oleksii-Kramar <dp020991kos@gmail.com>
2022-11-16 20:51:16 +0100

    WordPress_page-for-one-toy_commit

--- page-toys.php
+++ page-toys.php
@@ -41,7 +41,7 @@ Template Name: Игрушки
                                 <div class="toys__item-descr">
                                     <?php the_field('toys_desc'); ?>                            
                                 </div>
-                                <div class="minibutton toys__trigger">Подробнее</div>
+                                <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
                             </div>
                         </div>
                     
@@ -86,7 +86,7 @@ Template Name: Игрушки
                         <div class="toys__item-descr">
                             <?php the_field('toys_desc'); ?>    
                         </div>
-                        <div class="minibutton toys__trigger">Подробнее</div>
+                        <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
                     </div>
                 </div>
                 
