4f58883b8a716d87367b230f04f22d55332972f7
Oleksii-Kramar <dp020991kos@gmail.com>
2022-11-21 10:22:54 +0100

    WordPress_single-adu_commit

--- functions.php
+++ functions.php
@@ -21,6 +21,10 @@ function filter_nav_menu_link_attributes($atts, $item, $args) {
           if ($item->current) {
                $atts['class'] .= ' header__nav-item-active';
           }
+          // print_r($item);
+          if ($item->ID === 215 && ( in_category( 'soft_toys' ) || in_category( 'adu_toys' ))) {
+               $atts['class'] .= ' header__nav-item-active';
+          }
      };
      return $atts;
 }
